ca1785c992f917cb18818211124bae5d40c73f6b
jamiedewar <jdewar@legendboats.com>
2025-05-13 20:21:39 -0400

    Create used-boats-data.js

---
+++ docs/used-boats-data.js
@@ -0,0 +1,14 @@
+const usedBoats = [
+  {
+    "title": "2021 Legend 18 XTR",
+    "price": "$28,500",
+    "description": "Mint condition, low hours, ready to fish.",
+    "image": "https://your-image-url.com/boat1.jpg"
+  },
+  {
+    "title": "2019 Princecraft Vectra 21",
+    "price": "$34,000",
+    "description": "Great family pontoon with lots of space.",
+    "image": "https://your-image-url.com/boat2.jpg"
+  }
+];
